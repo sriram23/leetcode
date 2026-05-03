080a73dc2116b61c66a0604f008ad0971dd15287
Sriram <sriramsubramanian23@gmail.com>
2026-05-03 17:15:56 +0530

    Time: 4 ms (49.15%) | Memory: 57 MB (9.67%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,10 +4,10 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    const map =  new Map()
-    for(let i=0;i<nums.length;i++) {
-        const compl = target - nums[i]
-        if(map.has(compl)) return [map.get(compl), i]
+    const map = new Map()
+    for(let i=0;i<nums.length;i++){
+        let rem = target - nums[i]
+        if(map.has(rem)) return [map.get(rem), i]
         map.set(nums[i], i)
     }
 };
