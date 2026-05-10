325f29c71b2eeebae43ca9187917d22e6bf4d628
Sriram B <sriramsubramanian23@gmail.com>
2026-05-10 18:44:41 +0530

    Time: 8 ms (86.98%) | Memory: 59.2 MB (71.83%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -6,7 +6,7 @@
 var topKFrequent = function(nums, k) {
     const map = new Map()
     for(let i=0;i<nums.length;i++) {
-        if(map.has(nums[i])) map.set(nums[i], map.get(nums[i]) + 1)
+        if(map.has(nums[i])) map.set(nums[i], map.get(nums[i])+1)
         else map.set(nums[i], 1)
     }
     const sorted = [...map.entries()].sort((x,y) => y[1] - x[1]).slice(0, k)
