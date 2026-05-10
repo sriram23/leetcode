a70c3c943f85ecca574121f0460663e6a8b5b8d5
Sriram B <sriramsubramanian23@gmail.com>
2026-05-10 17:55:31 +0530

    Time: 5 ms (77.83%) | Memory: 72 MB (29.42%) - LeetSync

---
+++ 238-product-of-array-except-self/product-of-array-except-self.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var productExceptSelf = function(nums) {
+    const result = new Array(nums.length).fill(1)
+
+    let leftProduct = 1
+    for(let i=0;i<nums.length;i++) {
+        result[i] = leftProduct
+        leftProduct *= nums[i]
+    }
+
+    let rightProduct = 1
+    for(let i=nums.length-1;i>=0;i--) {
+        result[i] *= rightProduct
+        rightProduct *= nums[i]
+    }
+    return result
+};
