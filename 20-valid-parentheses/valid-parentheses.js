87021970cc04dd87df5a6965e58ca3a0d3728beb
Sriram B <sriramsubramanian23@gmail.com>
2026-05-11 22:15:47 +0530

    Time: 3 ms (69.82%) | Memory: 58.1 MB (8.71%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.js
+++ 20-valid-parentheses/valid-parentheses.js
@@ -3,19 +3,16 @@
  * @return {boolean}
  */
 var isValid = function(s) {
-    if(s.length % 2 !== 0) return false
-    const pairs = {
-        '[': ']',
-        '(': ')',
-        '{': '}'
-    }
     const stack = []
+    const pairs = {
+        '{': '}',
+        '[': ']',
+        '(': ')'
+    }
     for(let l of s) {
-        if(pairs[l])
-           stack.push(pairs[l])
-        else if(stack[stack.length-1] === l)
-            stack.pop()
+        if(pairs[l]) stack.push(pairs[l])
+        else if(stack[stack.length-1] === l) stack.pop()
         else stack.push(-1)
     }
-    return stack.length===0
+    return stack.length === 0
 };
