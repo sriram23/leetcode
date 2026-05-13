c6f9c6c6c7089bcc1f29bce86adc9d68a7a4ae43
Sriram B <sriramsubramanian23@gmail.com>
2026-05-13 19:43:24 +0530

    Time: 19 ms (47.96%) | Memory: 73.3 MB (81.77%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,6 +3,5 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const set = new Set(nums)
-    return !(nums.length === set.size)
+    return !(nums.length === new Set(nums).size)
 };
