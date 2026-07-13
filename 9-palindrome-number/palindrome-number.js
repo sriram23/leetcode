fc3e9d0b7d82ee5eb6343fbffc3aebc343501c2e
Sriram B <sriramsubramanian23@gmail.com>
2026-07-13 19:18:11 +0530

    Time: 8 ms (46.23%) | Memory: 65.4 MB (17.28%) - LeetSync

--- 9-palindrome-number/palindrome-number.js
+++ 9-palindrome-number/palindrome-number.js
@@ -3,5 +3,5 @@
  * @return {boolean}
  */
 var isPalindrome = function(x) {
-    return x === +x.toString().split("").reverse().join("")
+    return x<0 ? false : x === +x.toString().split("").reverse().join("")
 };
