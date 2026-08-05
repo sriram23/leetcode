b7bd66b35cdb60c30d46c666183ff1b0818c94e0
Sriram B <sriramsubramanian23@gmail.com>
2026-08-05 20:09:47 +0530

    Time: 0 ms (100.00%) | Memory: 60.7 MB (31.54%) - LeetSync

--- 283-move-zeroes/move-zeroes.js
+++ 283-move-zeroes/move-zeroes.js
@@ -3,9 +3,17 @@
  * @return {void} Do not return anything, modify nums in-place instead.
  */
 var moveZeroes = function(nums) {
-  const withoutZeros = nums.filter(n => n != 0)
-  for(let i=0;i<nums.length;i++) {
-    if(i<withoutZeros.length) nums[i] = withoutZeros[i]
-    else nums[i] = 0
-  }
+    // two pointer approach
+    let w = 0
+    // Rewriting the zeros with next number
+    for(let i=0;i<nums.length;i++) {
+        if(nums[i] !== 0) {
+            nums[w] = nums[i]
+            w++
+        }
+    }
+    // Rewriting the last digits with zeros
+    for(let i=w;i<nums.length;i++) {
+        nums[i] = 0
+    }
 };
