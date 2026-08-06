4d16e20e1d2fc1ec5a060b85118859a991325e57
Sriram B <sriramsubramanian23@gmail.com>
2026-08-06 10:28:22 +0530

    Time: 4 ms (5.21%) | Memory: 54 MB (71.45%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.js
+++ 88-merge-sorted-array/merge-sorted-array.js
@@ -6,6 +6,8 @@
  * @return {void} Do not return anything, modify nums1 in-place instead.
  */
 var merge = function(nums1, m, nums2, n) {
-    nums1.splice(m, n, ...nums2)
-    nums1.sort((x,y) => x-y)
+    for(let i=0;i<n;i++) {
+        nums1[m+i] = nums2[i]
+    }
+    nums1.sort((x, y) => x-y)
 };
