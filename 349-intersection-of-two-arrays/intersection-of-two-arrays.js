cfe188f0dc66f3c0e47f1f6c4438038a17725905
Sriram B <sriramsubramanian23@gmail.com>
2026-08-07 18:12:03 +0530

    Time: 2 ms (43.20%) | Memory: 55.3 MB (47.82%) - LeetSync

--- 349-intersection-of-two-arrays/intersection-of-two-arrays.js
+++ 349-intersection-of-two-arrays/intersection-of-two-arrays.js
@@ -7,8 +7,14 @@ var intersection = function(nums1, nums2) {
     const num1Set = new Set(nums1)
     const num2Set = new Set(nums2)
     const intersect = []
-    for(let v of num1Set) {
-        if(num2Set.has(v)) intersect.push(v)
+    if(num1Set.size < num2Set.size) {
+        for(let v of num1Set) {
+            if(num2Set.has(v)) intersect.push(v)
+        }
+    } else {
+        for(let v of num2Set) {
+            if(num1Set.has(v)) intersect.push(v)
+        }
     }
     return intersect
 };
