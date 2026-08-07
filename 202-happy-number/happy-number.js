b78bce5e5fd2faee2a03cef8a6973804460d7070
Sriram B <sriramsubramanian23@gmail.com>
2026-08-07 18:47:49 +0530

    Time: 1 ms (73.65%) | Memory: 54.1 MB (84.41%) - LeetSync

--- 202-happy-number/happy-number.js
+++ 202-happy-number/happy-number.js
@@ -2,21 +2,21 @@
  * @param {number} n
  * @return {boolean}
  */
-var isHappy = function(n) {
-    const hMap = {}
 
-    while(n !== 1) {
-        let current = n
-        let sum = 0;
-        while(current !== 0) {
-            sum += (current%10) ** 2
-            current = Math.floor(current/10)
-        }
-        if(hMap[sum]) {
-            return false
-        }
-        hMap[sum] = true
-        n = sum
+const getNumber = (n) => {
+    let res = 0
+    while(n>0) {
+        res += (Math.floor(n%10))**2
+        n = Math.floor(n/10)
     }
-    return true
+    return res
+}
+var isHappy = function(n) {
+    const temp = []
+    while(true) {
+        if(n === 1) return true
+        else if(temp.includes(n)) return false
+        temp.push(n)
+        n = getNumber(n)
+    }
 };
