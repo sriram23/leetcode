209302e7555ef32262c89cf15fab73842c31734b
Sriram B <sriramsubramanian23@gmail.com>
2026-08-07 19:08:49 +0530

    Time: 7 ms (28.70%) | Memory: 59.5 MB (12.23%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -5,8 +5,7 @@
 var majorityElement = function(nums) {
     const map = new Map()
     for(let n of nums) {
-        if(map.has(n)) map.set(n, map.get(n)+1)
-        else map.set(n, 1)
+        map.set(n, (map.get(n) || 0) + 1)
     }
     return map.entries().reduce((int, acc) => int[1] > acc[1]?int:acc)[0]
 };
