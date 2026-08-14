de3d58f6ec0eb9d04e0ef5bb7efea153047180b8
Sriram B <sriramsubramanian23@gmail.com>
2026-08-14 18:52:56 +0530

    Time: 0 ms (100.00%) | Memory: 54.9 MB (31.21%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.js
+++ 118-pascals-triangle/pascals-triangle.js
@@ -3,17 +3,16 @@
  * @return {number[][]}
  */
 var generate = function(numRows) {
+    if(numRows === 0) return []
     const arr = [[1]]
     for(let i=1;i<numRows;i++) {
-        if(arr[i-1]) {
-            const tArr = arr[i-1]
-            const temp = [1]
-            for(let j=0;j<tArr.length-1;j++) {
-                temp.push(tArr[j] + tArr[j+1])
-            }
-            temp.push(1)
-            arr.push(temp)
+        const tArr = arr[i-1]
+        const temp = [1]
+        for(let j=0;j<tArr.length-1;j++) {
+            temp.push(tArr[j] + tArr[j+1])
         }
+        temp.push(1)
+        arr.push(temp)
     }
     return arr
 };
