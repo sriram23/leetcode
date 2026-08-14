327ed9c126176ea146bdae7b60076e526db8fbc6
Sriram B <sriramsubramanian23@gmail.com>
2026-08-14 19:20:45 +0530

    Time: 1 ms (41.52%) | Memory: 53.9 MB (40.57%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.js
+++ 119-pascals-triangle-ii/pascals-triangle-ii.js
@@ -4,16 +4,15 @@
  */
 var getRow = function(rowIndex) {
     if(rowIndex === 0) return [1]
-    let prevRow = []
-    let currRow = []
+    let row = []
     for(let i=1;i<rowIndex+1;i++) {
-        prevRow = currRow
-        currRow = []
+        const currRow = []
         currRow.push(1)
-        for(let j=0;j<prevRow.length-1;j++) {
-            currRow.push(prevRow[j] + prevRow[j+1])
+        for(let j=0;j<row.length-1;j++) {
+            currRow.push(row[j] + row[j+1])
         }
         currRow.push(1)
+        row = currRow
     }
-    return currRow
+    return row
 };
