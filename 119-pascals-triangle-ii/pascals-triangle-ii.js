81e63709254d9d25c5b19ee1eebbf3fb1091af3f
Sriram B <sriramsubramanian23@gmail.com>
2026-08-14 19:17:59 +0530

    Time: 1 ms (41.52%) | Memory: 53.8 MB (40.57%) - LeetSync

--- 119-pascals-triangle-ii/pascals-triangle-ii.js
+++ 119-pascals-triangle-ii/pascals-triangle-ii.js
@@ -4,15 +4,16 @@
  */
 var getRow = function(rowIndex) {
     if(rowIndex === 0) return [1]
-    let pascalTriangle = [[1]]
+    let prevRow = []
+    let currRow = []
     for(let i=1;i<rowIndex+1;i++) {
-        const prevRow = pascalTriangle[i-1]
-        const currRow = [1]
+        prevRow = currRow
+        currRow = []
+        currRow.push(1)
         for(let j=0;j<prevRow.length-1;j++) {
             currRow.push(prevRow[j] + prevRow[j+1])
         }
         currRow.push(1)
-        pascalTriangle.push(currRow)
     }
-    return pascalTriangle[rowIndex]
+    return currRow
 };
